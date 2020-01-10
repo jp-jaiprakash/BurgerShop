a700cd950cedf2b880ab9f3c7838dcbba08feb2f
jprakas <jai_prakash@gap.com>
2020-01-10 07:24:03 +0530

    Till this commit we can show please add ingredients to user

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -4,12 +4,21 @@ import classes from './Burger.css';
 import BurgerIngredient from './BurgerIngredients/BurgerIngredients';
 const burger = (props) => {
     console.log(props);
-    const transformedIngredients = Object.keys(props.ingredients)
+    let transformedIngredients = Object.keys(props.ingredients)
                                     .map(igKey => {
                                         return [...Array(props.ingredients[igKey])].map((_,i) => {
                                             return <BurgerIngredient key={igKey+i} type={igKey}/>;
                                         })
                                     })
+                                    .reduce(
+                                        (arr, el) => {
+                                                return arr.concat(el)
+                                        }, []
+                                    );
+    if (transformedIngredients.length === 0){
+        transformedIngredients = <p>Please start adding elements</p>
+    }
+    console.log(transformedIngredients);
     return (
         <div className={classes.Burger}>
             <BurgerIngredient type="bread-top"/>
